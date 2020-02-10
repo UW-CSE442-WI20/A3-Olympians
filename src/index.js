de6d89f285583f02c9d9f4b412a27d0931aefa13
Melissa B <mbirch2@uw.edu>
2020-02-09 23:41:17 -0800

    moved athlete chart to upper right, centered medal bars

--- src/index.js
+++ src/index.js
@@ -382,7 +382,7 @@ function generateAthleteChart(data) {
   smallsvg.selectAll("text").remove();
   smallsvg.selectAll("g").transition();//.delay(5000);
 
-  var smallWidth = 1200;
+  var smallWidth = 800;
   var smallHeight = 500;
 
   var smallMargin = {
@@ -392,75 +392,174 @@ function generateAthleteChart(data) {
       bottom: 30
   };
 
-  var innerSmallWidth = smallWidth - smallMargin.left - smallMargin.right;
-  var innerSmallHeight = smallHeight - smallMargin.top - smallMargin.bottom;
+    var innerSmallWidth = smallWidth - smallMargin.left - smallMargin.right;
+    var innerSmallHeight = smallHeight - smallMargin.top - smallMargin.bottom;
 
-  var x = d3.scaleLinear().rangeRound([0, smallWidth], 0.5);
-  var y = d3.scaleLinear().rangeRound([smallHeight, 0]);
+    var x = d3.scaleLinear().rangeRound([0, smallWidth], 0.5);
+    var y = d3.scaleLinear().rangeRound([smallHeight, 0]);
 
-  x.domain(data.map(function(d) { return d.year; }));
-  y.domain([0, d3.max(groupData, function(key) { return d3.max(key.values, function(d) { return d.grpValue; }); })]);
+    x.domain(data.map(function(d) { return d.year; }));
+    y.domain([0, d3.max(groupData, function(key) { return d3.max(key.values, function(d) { return d.grpValue; }); })]);
 
-  const xSmallScale = d3.scaleTime().domain([(+data[0].Start - 4), (+data[0].End + 4)]).range([smallMargin["left"], innerSmallWidth]);
-  const ySmallScale = d3.scaleLinear().domain([10,0]).range([smallMargin["bottom"], innerSmallHeight]);
+    const xSmallScale = d3.scaleTime().domain([(+data[0].Start - 4), (+data[0].End + 4)]).range([smallMargin["left"], innerSmallWidth]);
+    const ySmallScale = d3.scaleLinear().domain([10,0]).range([smallMargin["bottom"], innerSmallHeight]);
 
-  const getTickValues = (startTick, endTick) => {
-      var values = [];
-      for (var i = startTick; i <= endTick; i+=4) { values.push(i) }
-      return values;
-  }
+    const getTickValues = (startTick, endTick) => {
+        var values = [];
+        for (var i = startTick; i <= endTick; i+=4) { values.push(i) }
+        return values;
+    }
 
-  const xSmallAxis = d3.axisBottom(xSmallScale)
-      .tickPadding(30)
-      .tickValues(getTickValues(+data[0].Start, +data[0].End))
-      .tickFormat(d3.format("Y"))
-      .tickSize(-innerSmallHeight);
-  const ySmallAxis = d3.axisLeft(ySmallScale)
-      .tickSize(-innerSmallWidth);
+    const xSmallAxis = d3.axisBottom(xSmallScale)
+        .tickPadding(30)
+        .tickValues(getTickValues(+data[0].Start, +data[0].End))
+        .tickFormat(d3.format("Y"))
+        // .tickSize(-innerSmallHeight);
+    const ySmallAxis = d3.axisLeft(ySmallScale)
+        .tickSize(-innerSmallWidth);
 
-   smallsvg.append("text")
-       .attr("x", innerSmallWidth / 2 )
-       .attr("y", smallHeight - innerSmallHeight)
-       .style("text-anchor", "middle")
-       .text(data[0].Name);
+    // add title: athlete name and country
+     smallsvg.append("text")
+         .attr("x", smallWidth / 2 )
+         .attr("y", smallHeight - innerSmallHeight - 1.3 * smallMargin["top"])
+         .style("text-anchor", "middle")
+         .text(data[0].Name + "  (" + data[0].NOC + ")");
 
     // add axis groups to smallsvg
     const xSmallAxisGroup = smallsvg.append("g")
-       .attr("class", "axis x")
-       .attr("transform", "translate(0," + innerSmallHeight + ")")
-       .call(xSmallAxis);
+        .attr("class", "axis x")
+        .attr("transform", "translate(0," + innerSmallHeight + ")")
+        .call(xSmallAxis);
     const ySmallAxisGroup = smallsvg.append("g")
-       .attr("class", "axis y")
-       .attr("transform", "translate(" + smallMargin["left"] + ",0)")
-       .call(ySmallAxis);
+        .attr("class", "axis y")
+        .attr("transform", "translate(" + smallMargin["left"] + ",0)")
+        .call(ySmallAxis);
 
     var x1  = d3.scaleBand();
     var medalTypes = groupData[0].values.map(function(d) { return d.grpName; });
     x1.domain(medalTypes).rangeRound([0, 30]);//x.bandwidth()]) ;
 
     var slice = smallsvg.selectAll(".slice")
-       .data(groupData)
-       .enter().append("g")
-       .attr("class", "g")
-       .attr("transform",function(d) { return "translate(" + xSmallScale(d.key) + ",0)"; });
+        .data(groupData)
+        .enter().append("g")
+        .attr("class", "g")
+        .attr("transform",function(d) { return "translate(" + xSmallScale(d.key) + ",0)"; });
 
-   var color = d3.scaleOrdinal()
-       .range(["#CD7F32","#C0C0C0","#D4AF37"]);
+    var color = d3.scaleOrdinal()
+        .range(["#CD7F32","#C0C0C0","#D4AF37"]);
 
-   slice.selectAll("rect")
-       .data(function(d) { return d.values; })
-       .enter().append("rect")
-       .attr("width", x1.bandwidth())
-       .style("fill", function(d) { return color(d.grpName) })
-       .attr("x", function(d) { return x1(d.grpName); })
-       .attr("y", function(d) { return ySmallScale(d.grpValue) })
-       .attr("height", function(d) {return innerSmallHeight - ySmallScale(d.grpValue); });
+    const medalOffset = d => {
 
-   data.forEach(function(d) {
-       d.year = d.Year;
-       d.medal = d.Medal;
-       console.log(d.Name + " -- " + d.year + ", " + d.medal);
-   });
+    }
+
+    slice.selectAll("rect")
+        .data(function(d) { return d.values; })
+        .enter().append("rect")
+        .attr("width", x1.bandwidth())
+        .style("fill", function(d) { return color(d.grpName) })
+        .attr("x", function(d) { return x1(d.grpName) - (1.5 * x1.bandwidth()); })
+        .attr("y", function(d) { return ySmallScale(d.grpValue) })
+        .attr("height", function(d) {return innerSmallHeight - ySmallScale(d.grpValue); });
+
+  // now add titles to the axes
+    smallsvg.append("text")
+        .attr("text-anchor", "middle")  // this makes it easy to centre the text as the transform is applied to the anchor
+        .attr("transform", "translate("+ (smallMargin["left"]/2) +","+(smallHeight/2)+")rotate(-90)")  // text is drawn off the screen top left, move down and out and rotate
+        .text("Number of Medals Won");
+
+    smallsvg.append("text")
+        .attr("text-anchor", "middle")  // this makes it easy to centre the text as the transform is applied to the anchor
+        .attr("transform", "translate("+ (smallWidth/2) +","+(smallHeight)+")")  // centre below axis
+        .text("Year Competed");
+
+    data.forEach(function(d) {
+        d.year = d.Year;
+        d.medal = d.Medal;
+        console.log(d.Name + " -- " + d.year + ", " + d.medal);
+    });
+
+
+  // var innerSmallWidth = smallWidth - smallMargin.left - smallMargin.right;
+  // var innerSmallHeight = smallHeight - smallMargin.top - smallMargin.bottom;
+  //
+  // var x = d3.scaleLinear().rangeRound([0, smallWidth], 0.5);
+  // var y = d3.scaleLinear().rangeRound([smallHeight, 0]);
+  //
+  // x.domain(data.map(function(d) { return d.year; }));
+  // y.domain([0, d3.max(groupData, function(key) { return d3.max(key.values, function(d) { return d.grpValue; }); })]);
+  //
+  // const xSmallScale = d3.scaleTime().domain([(+data[0].Start - 4), (+data[0].End + 4)]).range([smallMargin["left"], innerSmallWidth]);
+  // const ySmallScale = d3.scaleLinear().domain([10,0]).range([smallMargin["bottom"], innerSmallHeight]);
+  //
+  // const getTickValues = (startTick, endTick) => {
+  //     var values = [];
+  //     for (var i = startTick; i <= endTick; i+=4) { values.push(i) }
+  //     return values;
+  // }
+  //
+  // const xSmallAxis = d3.axisBottom(xSmallScale)
+  //     .tickPadding(30)
+  //     .tickValues(getTickValues(+data[0].Start, +data[0].End))
+  //     .tickFormat(d3.format("Y"))
+  //     // .tickSize(-innerSmallHeight);
+  // const ySmallAxis = d3.axisLeft(ySmallScale)
+  //     .tickSize(-innerSmallWidth);
+  //
+  //  // add title: athlete name and country
+  //  smallsvg.append("text")
+  //      .attr("x", smallWidth / 2 )
+  //      .attr("y", smallHeight - innerSmallHeight - smallMargin["top"])
+  //      .style("text-anchor", "middle")
+  //      .text(data[0].Name + "  (" + data[0].NOC + ")");
+  //
+  //   // add axis groups to smallsvg
+  //   const xSmallAxisGroup = smallsvg.append("g")
+  //      .attr("class", "axis x")
+  //      .attr("transform", "translate(0," + innerSmallHeight + ")")
+  //      .call(xSmallAxis);
+  //   const ySmallAxisGroup = smallsvg.append("g")
+  //      .attr("class", "axis y")
+  //      .attr("transform", "translate(" + smallMargin["left"] + ",0  )")
+  //      .call(ySmallAxis);
+  //
+  //   var x1  = d3.scaleBand();
+  //   var medalTypes = groupData[0].values.map(function(d) { return d.grpName; });
+  //   x1.domain(medalTypes).rangeRound([0, 30]);//x.bandwidth()]) ;
+  //
+  //   var slice = smallsvg.selectAll(".slice")
+  //      .data(groupData)
+  //      .enter().append("g")
+  //      .attr("class", "g")
+  //      .attr("transform",function(d) { return "translate(" + xSmallScale(d.key) + ",0)"; });
+  //
+  //  var color = d3.scaleOrdinal()
+  //      .range(["#CD7F32","#C0C0C0","#D4AF37"]);
+  //
+  //  slice.selectAll("rect")
+  //      .data(function(d) { return d.values; })
+  //      .enter().append("rect")
+  //      .attr("width", x1.bandwidth())
+  //      .style("fill", function(d) { return color(d.grpName) })
+  //      .attr("x", function(d) { return x1(d.grpName); })
+  //      .attr("y", function(d) { return ySmallScale(d.grpValue) })
+  //      .attr("height", function(d) {return innerSmallHeight - ySmallScale(d.grpValue); });
+  //
+  //   // now add titles to the axes
+  //   smallsvg.append("text")
+  //       .attr("text-anchor", "middle")  // this makes it easy to centre the text as the transform is applied to the anchor
+  //       .attr("transform", "translate("+ (smallMargin["left"]/2) +","+(smallHeight/2)+")rotate(-90)")  // text is drawn off the screen top left, move down and out and rotate
+  //       .text("Number of Medals Won");
+  //
+  //   smallsvg.append("text")
+  //       .attr("text-anchor", "middle")  // this makes it easy to centre the text as the transform is applied to the anchor
+  //       .attr("transform", "translate("+ (smallWidth/2) +","+(smallHeight-(smallMargin["bottom"]/3))+")")  // centre below axis
+  //       .text("Year Competed");
+  //
+  //  data.forEach(function(d) {
+  //      d.year = d.Year;
+  //      d.medal = d.Medal;
+  //      console.log(d.Name + " -- " + d.year + ", " + d.medal);
+  //  });
 }
 
 // initialize the timeslider
