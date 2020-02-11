0dfb4a5d3cc23790c85aea489f67a736eb01b104
M Lin <lin-family@live.com>
2020-02-10 17:23:08 -0800

    Add search bar with suggestions

--- src/index.js
+++ src/index.js
@@ -40,6 +40,7 @@ const csvFile = require('../data/olympic_overall.csv');
 var medalCounts;
 var maxMedals;
 var selectedValues;
+var peopleNames = [];
 // data structures to be loaded in
 
 // make the Name the key
@@ -352,6 +353,7 @@ function setupNOCFiltering(data) {
 
     }
 
+    peopleNames = [];
     for (var i = 0; i < selectedValues.length; i++) {
       console.log("drawing: ", i);
       redraw(filterByMedal(entriesByNOC[selectedValues[i]], medalCounts, medals));
@@ -402,6 +404,7 @@ function setupMedalFiltering(data) {
     .on("input", function () {
       chart.selectAll("line").remove();
       chart.selectAll("circle").remove();
+      peopleNames = [];
       for (let i = 0; i < selectedValues.length; i++) {
         redraw(filterByMedal(entriesByNOC[selectedValues[i]], medalCounts, this.value));
       }
@@ -421,6 +424,7 @@ function filterByMedal(data, medalCounts, nMedals) {
   for (var person in medalCounts) {
     if (medalCounts[person] >= nMedals) {
       currMedals.push(person);
+      peopleNames.push(person);
     }
   }
   let currPeople = _.filter(data.values, (item) => {
@@ -434,6 +438,117 @@ function filterByMedal(data, medalCounts, nMedals) {
   return currPeople[0];
 }
 
+function autocomplete(input) {
+  /*the autocomplete function takes two arguments,
+  the text field element and an array of possible autocompleted values:*/
+  var currentFocus;
+  /*execute a function when someone writes in the text field:*/
+  input.addEventListener("input", function(e) {
+    // a is the autocomplete outer div element
+    // b is the temporary variable used to store each option in the div
+    var a, b, val = this.value;
+    /*close any already open lists of autocompleted values*/
+    closeAllLists();
+    if (!val) { 
+      return false;
+    }
+    currentFocus = -1;
+    /*create a DIV element that will contain the items (values):*/
+    a = document.createElement("DIV");
+    a.setAttribute("id", this.id + "autocomplete-list");
+    a.setAttribute("class", "autocomplete-items");
+    /*append the DIV element as a child of the autocomplete container:*/
+    this.parentNode.appendChild(a);
+    /*for each item in the array...*/
+    for (var i = 0; i < peopleNames.length; i++) {
+      /*check if the item starts with the same letters as the text field value:*/
+      if (peopleNames[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+        /*create a DIV element for each matching element:*/
+        b = document.createElement("DIV");
+        /*make the matching letters bold:*/
+        b.innerHTML = "<strong>" + peopleNames[i].substr(0, val.length) + "</strong>";
+        b.innerHTML += peopleNames[i].substr(val.length);
+        /*insert a input field that will hold the current array item's value:*/
+        b.innerHTML += "<input type='hidden' value='" + peopleNames[i] + "'>";
+        /*execute a function when someone clicks on the item value (DIV element):*/
+        b.addEventListener("click", function(e) {
+          /*insert the value for the autocomplete text field:*/
+          input.value = this.getElementsByTagName("input")[0].value;
+          /*close the list of autocompleted values,
+          (or any other open lists of autocompleted values:*/
+          closeAllLists();
+          generateAthleteChart( _.find(d3.values(entriesByName), function (item) { return item.key === input.value; }).values);
+        });
+        a.appendChild(b);
+      }
+    }
+  });
+  /*execute a function presses a key on the keyboard:*/
+  input.addEventListener("keydown", function(e) {
+    var currSuggestion = document.getElementById(this.id + "autocomplete-list");
+    if (currSuggestion) {
+      currSuggestion = currSuggestion.getElementsByTagName("div");
+    }
+    if (e.keyCode == 40) {
+      /*If the arrow DOWN key is pressed,
+      increase the currentFocus variable:*/
+      currentFocus++;
+      /*and and make the current item more visible:*/
+      addActive(currSuggestion);
+    } else if (e.keyCode == 38) { //up
+      /*If the arrow UP key is pressed,
+      decrease the currentFocus variable:*/
+      currentFocus--;
+      /*and and make the current item more visible:*/
+      addActive(currSuggestion);
+    } else if (e.keyCode == 13) {
+      /*If the ENTER key is pressed, prevent the form from being submitted,*/
+      e.preventDefault();
+      if (currentFocus > -1) {
+        /*and simulate a click on the "active" item:*/
+        if (currSuggestion) {
+          currSuggestion[currentFocus].click();
+        }
+      }
+    }
+  });
+  function addActive(item) {
+    /*a function to classify an item as "active":*/
+    if (!item) {
+      return false;
+    }
+    /*start by removing the "active" class on all items:*/
+    removeActive(item);
+    if (currentFocus >= item.length) {
+      currentFocus = 0;
+    }
+    if (currentFocus < 0) {
+      currentFocus = (item.length - 1);
+    }
+    /*add class "autocomplete-active":*/
+    item[currentFocus].classList.add("autocomplete-active");
+  }
+  function removeActive(item) {
+    /*a function to remove the "active" class from all autocomplete items:*/
+    for (var i = 0; i < item.length; i++) {
+      item[i].classList.remove("autocomplete-active");
+    }
+  }
+  function closeAllLists(elmnt) {
+    /*close all autocomplete lists in the document,
+    except the one passed as an argument:*/
+    var x = document.getElementsByClassName("autocomplete-items");
+    for (var i = 0; i < x.length; i++) {
+      if (elmnt != x[i] && elmnt != input) {
+        x[i].parentNode.removeChild(x[i]);
+      }
+    }
+  }
+  /*execute a function when someone clicks in the document:*/
+  document.addEventListener("click", function (e) {
+      closeAllLists(e.target);
+  });
+} 
 
 // function to generate individual athlete chart
 // as a bar chart of medals over time
@@ -714,4 +829,5 @@ d3.csv(csvFile).then(function (data) {
   initializeTimeSlider();
   // initialize/create all the dropdowns/filters that will be shown in the view
   initializeOptions(data);
+  autocomplete(document.getElementById("searchbar"));
 });
