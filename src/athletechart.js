799d362223dfc338d943ef373f7c37c99163caae
Melissa B <mbirch2@uw.edu>
2020-02-12 15:46:49 -0800

    added swim events on medal hover

--- src/athletechart.js
+++ src/athletechart.js
@@ -12,14 +12,14 @@ const _ = require("underscore");
 export function generateAthleteChart(data) {
 
     const containsYear = (groups, year) => {
-        return _.find(d3.values(groups), function(item) {
+        return _.find(d3.values(groups), function (item) {
             return item.key === year;
         });
     }
 
-    const getMedalCount = (medal, year) => {
+    const getMedalCount = (year, medal) => {
         var numMedals = 0;
-        data.forEach(function(item) {
+        data.forEach(function (item) {
             if (item.Medal === medal && item.Year === year) numMedals++;
         });
         return numMedals;
@@ -27,34 +27,50 @@ export function generateAthleteChart(data) {
 
     const groupData = [];
 
-    data.forEach(function(item) {
-        var bronze = getMedalCount('Bronze', item.Year);
-        var silver = getMedalCount('Silver', item.Year);
-        var gold = getMedalCount('Gold', item.Year);
+    const getEvents = (year, medal) => {
+        var events = [];
+        data.forEach(function (item) {
+            if (item.Year === year && item.Medal === medal) {
+                events.push(item.Event);
+            }
+        })
+        return events;
+    }
+
+    data.forEach(function (item) {
+        var bronze = getMedalCount(item.Year, 'Bronze');
+        var silver = getMedalCount(item.Year, 'Silver');
+        var gold = getMedalCount(item.Year, 'Gold');
+
+        var bronzeEvents = getEvents(item.Year, 'Bronze');
+        var silverEvents = getEvents(item.Year, 'Silver');
+        var goldEvents = getEvents(item.Year, 'Gold');
 
         if (containsYear(groupData, item.Year) === undefined) {
             var medalMap = [];
             for (let i = 1; i <= bronze; i++) {
-                medalMap.push({grpName:'Bronze', grpValue:i});
+                medalMap.push({grpName: 'Bronze', grpValue: i, grpEvent: bronzeEvents[i - 1]});
             }
             for (let i = 1; i <= silver; i++) {
-                medalMap.push({grpName:'Silver', grpValue:i});
+                medalMap.push({grpName: 'Silver', grpValue: i, grpEvent: silverEvents[i - 1]});
             }
             for (let i = 1; i <= gold; i++) {
-                medalMap.push({grpName:'Gold', grpValue:i});
+                medalMap.push({grpName: 'Gold', grpValue: i, grpEvent: goldEvents[i - 1]});
             }
 
             groupData.push(new Object(
-                { key: item.Year, values: medalMap
-                }))}});
+                {
+                    key: item.Year, values: medalMap
+                }))
+        }
+    });
 
     var smallsvg = d3.select("#small-chart");
 
-    // ideally want to have the medals shrink to 0, update axes and redraw everything
-    smallsvg.selectAll("g").transition(); //.delay(5000);
+    smallsvg.selectAll("g").transition();
     smallsvg.selectAll("g").remove();
     smallsvg.selectAll("text").remove();
-    smallsvg.selectAll("g").transition(); //.delay(5000);
+    smallsvg.selectAll("g").transition();
 
     var smallWidth = 500;
     var smallHeight = 400;
@@ -72,17 +88,16 @@ export function generateAthleteChart(data) {
     var x = d3.scaleLinear().rangeRound([0, smallWidth], 0.5);
     var y = d3.scaleLinear().rangeRound([smallHeight, 0]);
 
-    x.domain(data.map(function(d) {
+    x.domain(data.map(function (d) {
         return d.year;
     }));
-    y.domain([0, d3.max(groupData, function(key) {
-        return d3.max(key.values, function(d) {
+    y.domain([0, d3.max(groupData, function (key) {
+        return d3.max(key.values, function (d) {
             return d.grpValue;
         });
     })]);
 
-    const getXDomain = () =>
-    {
+    const getXDomain = () => {
         var domain = [];
         for (let i = +data[0].Start - 4; i <= +data[0].End + 4; i += 4) {
             domain.push(i);
@@ -90,7 +105,6 @@ export function generateAthleteChart(data) {
         return domain;
     }
 
-    // const xSmallScale = d3.scaleTime().domain([(+data[0].Start - 4), (+data[0].End + 4)]).range([smallMargin.left, innerSmallWidth]);
     const xSmallScale = d3.scaleBand().domain(getXDomain()).range([smallMargin.left, innerSmallWidth]);
     const ySmallScale = d3.scaleLinear().domain([10, 0]).range([smallMargin.bottom, innerSmallHeight]);
 
@@ -107,7 +121,6 @@ export function generateAthleteChart(data) {
         .tickValues(getTickValues(+data[0].Start, +data[0].End))
         .tickFormat(d3.format("Y"))
     const ySmallAxis = d3.axisLeft(ySmallScale)
-    // .tickSize(-innerSmallWidth);
 
     // add title: athlete name and country
     smallsvg.append("text")
@@ -121,43 +134,26 @@ export function generateAthleteChart(data) {
         .attr("class", "axis x")
         .attr("transform", "translate(0," + innerSmallHeight + ")")
         .call(xSmallAxis);
-    // const ySmallAxisGroup = smallsvg.append("g")
-    //     .attr("class", "axis y")
-    //     .attr("transform", "translate(" + smallMargin["left"] + ",0)")
-    //     .call(ySmallAxis);
 
     var x1 = d3.scaleBand();
-    var medalTypes = groupData[0].values.map(function(d) {
+    var medalTypes = groupData[0].values.map(function (d) {
         return d.grpName;
     });
-    x1.domain(medalTypes).rangeRound([0, 30]); //x.bandwidth()]) ;
+    x1.domain(medalTypes).rangeRound([0, 30]);
 
     var slice = smallsvg.selectAll(".slice")
         .data(groupData)
         .enter().append("g")
         .attr("class", "g")
-        .attr("transform", function(d) {
+        .attr("transform", function (d) {
             return "translate(" + xSmallScale(d.key) + ",0)";
         });
-
-    // var color = d3.scaleOrdinal()
-    //     .range(["#CD7F32","#C0C0C0","#D4AF37"]);
 
     var color = medalType => {
         if (medalType === 'Bronze') return "#CD7F32";
         else if (medalType === 'Silver') return "#C0C0C0";
         else return "#D4AF37";
     }
-
-    // CHANGE RECT TO STACKED CIRCLES
-    // slice.selectAll("rect")
-    //     .data(function(d) { return d.values; })
-    //     .enter().append("rect")
-    //     .attr("width", x1.bandwidth())
-    //     .style("fill", function(d) { return color(d.grpName) })
-    //     .attr("x", function(d) { return x1(d.grpName) - (1.5 * x1.bandwidth()); })
-    //     .attr("y", function(d) { return ySmallScale(d.grpValue) })
-    //     .attr("height", function(d) {return innerSmallHeight - ySmallScale(d.grpValue); });
 
     var cxOffset = medalType => {
         if (medalType === 'Bronze') {
@@ -170,12 +166,36 @@ export function generateAthleteChart(data) {
     }
 
     slice.selectAll("circle")
-        .data(function(d) { return d.values; })
+        .data(function (d) {
+            return d.values;
+        })
         .enter().append("circle")
-        .style("fill", function(d) { return color(d.grpName) })
-        .attr("cx", function(d) { return cxOffset(d.grpName) * xSmallScale.bandwidth(); })
-        .attr("cy", function(d) { return ySmallScale(d.grpValue - 1.5) - 30 })
-        .attr("r", 10);
+        .style("fill", function (d) {
+            return color(d.grpName)
+        })
+        .attr("cx", function (d) {
+            return cxOffset(d.grpName) * xSmallScale.bandwidth();
+        })
+        .attr("cy", function (d) {
+            return ySmallScale(d.grpValue - 1.5) - 30
+        })
+        .attr("r", 10)
+        .on("mouseover", function (d) {//Get this circle's x/y values, then augment for the tooltip
+            //Create the tooltip label
+            smallsvg.append("text")
+                .attr("id", "tooltip")
+                .attr("text-anchor", "middle")
+                .attr("transform", "translate(" + (smallWidth / 2) + "," + (innerSmallHeight / 3) + ")")
+                .attr("font-family", "sans-serif")
+                .attr("font-size", "11px")
+                .attr("font-weight", "bold")
+                .attr("fill", "black")
+                .style("pointer-events", "none")
+                .text(d.grpEvent);
+        })
+        .on("mouseout", function () {// Remove the tooltip
+            d3.select("#tooltip").remove();
+        });
 
     // now add titles to the axes
     smallsvg.append("text")
